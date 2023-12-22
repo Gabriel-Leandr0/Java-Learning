3e3a0d2eeb9f3f3caff59c3ebde225b3e7ef5b3a
gabriel-leandr0 <gabriel.leandrods@gmail.com>
2023-12-22 00:40:55 -0300

    resolvendo conflito do git

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -5,50 +7,81 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        Aluno aluno = new Aluno();
+        List<Aluno> alunoList = new ArrayList<Aluno>();
 
-        aluno.setNome("Gabriel");
-        aluno.setDataNascimento("09-09-2000");
+        System.out.println("Digite a quantidade de alunos : ");
+        final int qtdAluno = scanner.nextInt();
 
-        System.out.println("Digite a quantidade de disciplinas: ");
+        for (int posAluno = 1; posAluno <= qtdAluno; posAluno++) {
 
-        final int qtdDisciplina = scanner.nextInt();
+            Aluno aluno = new Aluno();
+            System.out.println("Digite o nome do aluno "+posAluno+" : ");
+            aluno.setNome(scanner.next());
+            System.out.println("Digite a data de nascimento do aluno "+aluno.getNome()+" : ");
+            aluno.setDataNascimento(scanner.next());
 
-        for (int pos = 1; pos <= qtdDisciplina; pos++) {
+            alunoList.add(aluno);
 
-            Disciplina disciplina = new Disciplina();
-            System.out.println("Digite o nome da Disciplina " + pos + " : ");
-            disciplina.setNomeDisciplina(scanner.next());
-            System.out.println("Digite a nota da disciplina " + disciplina.getNomeDisciplina() + " : ");
-            disciplina.setNota(Double.parseDouble(scanner.next()));
+            System.out.println("Digite a quantidade de disciplinas: ");
 
-            aluno.getDisciplinaList().add(disciplina);
-        }
+            final int qtdDisciplina = scanner.nextInt();
 
-        System.out.println("Deseja remover alguma disciplina? (1 - Sim / 0 - Não)");
-        int resposta = scanner.nextInt();
+            for (int posDisciplina = 1; posDisciplina <= qtdDisciplina; posDisciplina++) {
 
-        while (resposta == 1) {
-            for (int i = 0; i < aluno.getDisciplinaList().size(); i++) {
-                Disciplina disciplina = aluno.getDisciplinaList().get(i);
-                System.out.println("Posição " + i + ": " + disciplina.getNomeDisciplina());
+                Disciplina disciplina = new Disciplina();
+                System.out.println("Digite o nome da Disciplina " + posDisciplina + " : ");
+                disciplina.setNomeDisciplina(scanner.next());
+                System.out.println("Digite a nota da disciplina " + disciplina.getNomeDisciplina() + " : ");
+                disciplina.setNota(Double.parseDouble(scanner.next()));
+
+                aluno.getDisciplinaList().add(disciplina);
+
+                //alunoList.get(aluno.getDisciplinaList().add(disciplina));
             }
 
-            System.out.println("Digite o número da disciplina que deseja remover:");
-            int posicaoRemover = scanner.nextInt();
+            System.out.println("Deseja remover alguma disciplina? (1 - Sim / 0 - Não)");
+            int resposta = scanner.nextInt();
 
-            // Remover a disciplina da lista
-            if (posicaoRemover >= 0 && posicaoRemover < aluno.getDisciplinaList().size()) {
-                aluno.getDisciplinaList().remove(posicaoRemover);
-                System.out.println("Disciplina removida com sucesso!");
-            } else {
-                System.out.println("Posição inválida. Nenhuma disciplina removida.");
+            while (resposta == 1) {
+                for (int i = 0; i < aluno.getDisciplinaList().size(); i++) {
+                    Disciplina disciplina = aluno.getDisciplinaList().get(i);
+                    System.out.println("Posição " + i + ": " + disciplina.getNomeDisciplina());
+                }
+
+                System.out.println("Digite o número da disciplina que deseja remover:");
+                int posicaoRemover = scanner.nextInt();
+
+                // Remover a disciplina da lista
+                if (posicaoRemover >= 0 && posicaoRemover < aluno.getDisciplinaList().size()) {
+                    aluno.getDisciplinaList().remove(posicaoRemover);
+                    System.out.println("Disciplina removida com sucesso!");
+                } else {
+                    System.out.println("Posição inválida. Nenhuma disciplina removida.");
+                }
+
+                System.out.println("Deseja remover mais alguma disciplina? (1 - Sim / 0 - Não)");
+                resposta = scanner.nextInt();
             }
 
-            System.out.println("Deseja remover mais alguma disciplina? (1 - Sim / 0 - Não)");
-            resposta = scanner.nextInt();
         }
+        for (int i = 0; i < alunoList.size(); i++) {
 
+            Aluno aluno = alunoList.get(i);
 
+            System.out.println("----------------------------------------------");
+
+            System.out.println("ID do aluno : "+i);
+            System.out.println("Nome do aluno : "+aluno.getNome());
+            System.out.println("Data de nascimento : "+aluno.getDataNascimento());
+            // Iterar sobre a lista de disciplinas do aluno
+            System.out.println("Disciplinas:");
+
+            for (Disciplina disciplina : aluno.getDisciplinaList()) {
+                System.out.println("  - " + disciplina.getNomeDisciplina() + "Nota : "+disciplina.getNota());
+            }
+            System.out.println("Media : "+aluno.getMediaNota());
+
+            System.out.println("----------------------------------------------");
+        }
     }
 }
